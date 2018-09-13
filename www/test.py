0acca8b9df7bf4b46ebb8505f882a206ed8d8dc5
hcyCoding <yuanhaichun1993@163.com>
2018-09-13 14:01:24 +0800

    modify orm

---
+++ www/test.py
@@ -0,0 +1,19 @@
+
+
+import orm
+from models import User,Blog,Comment
+import asyncio
+
+
+async def test(loop):
+    await orm.create_pool(loop,user='root', password='123456', db='awesome')
+    u = User(name='Test', email='test5@example.com', passwd='1234567890', image='about:blank')
+    await u.save()
+
+async def findall(loop):
+    await orm.create_pool(loop,user='root', password='123456', db='awesome')
+    rs=await User.findAll()
+    print(rs)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(findall(loop))
